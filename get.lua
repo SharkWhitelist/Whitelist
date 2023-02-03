b625cbcf8fccd9c04f6ef9cfb3bf0b5d40c2bc4b
SharkWhitelist <124364774+SharkWhitelist@users.noreply.github.com>
2023-02-03 19:40:03 +0700

    Update and rename README.md to get.lua

---
+++ get.lua
@@ -0,0 +1,21 @@
+{
+  "args": {}, 
+  "headers": {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
+    "Accept-Encoding": "gzip, deflate, br", 
+    "Accept-Language": "en-US,en;q=0.9", 
+    "Host": "httpbin.org", 
+    "Sec-Ch-Ua": "\"Not_A Brand\";v=\"99\", \"Microsoft Edge\";v=\"109\", \"Chromium\";v=\"109\"", 
+    "Sec-Ch-Ua-Mobile": "?0", 
+    "Sec-Ch-Ua-Platform": "\"Windows\"", 
+    "Sec-Fetch-Dest": "document", 
+    "Sec-Fetch-Mode": "navigate", 
+    "Sec-Fetch-Site": "none", 
+    "Sec-Fetch-User": "?1", 
+    "Upgrade-Insecure-Requests": "1", 
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70", 
+    "X-Amzn-Trace-Id": "Root=1-63dd0077-191aeff7554d7ab92d5d39c1"
+  }, 
+  "origin": "49.228.81.155", 
+  "url": "https://httpbin.org/get"
+}
